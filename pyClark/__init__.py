e8b1e5a1c4d0445934fc18d0ca8c2c154af62636
Podshot <ben.gothard3@gmail.com>
2018-03-30 19:27:05 -0400

    There can now be multiple Clark objects and added in an anonymizer function

--- pyClark/__init__.py
+++ pyClark/__init__.py
@@ -1,3 +1,5 @@
-from .clark import WatchDog
+from .clark import Clark
+from .utils import anonymize
+global_clark = None
 
 __version__ = '0.0.1.dev1'
--- pyClark/clark.py
+++ pyClark/clark.py
@@ -1,25 +1,30 @@
+import sys
 import json
 import types
 import requests
+import logging
+from . import utils
 
-class WatchDog(object):
+logger = logging.getLogger(__name__)
 
-    _ready = False
-    _hostname = None
+class Clark(object):
 
-    @classmethod
-    def _suitup(cls, hostname, *args, **kwargs):
-        if cls._ready:
-            return
-        cls._ready = True
-        cls._hostname = hostname
+    _already_injected = False
+    _object_serializers = {}
 
+    def __init__(self, hostname, inject=False, **kwargs):
+        self._hostname = hostname
+        if inject and not self._already_injected:
+            module = sys.modules[__name__.split('.')[0]]
+            setattr(module, 'global_clark', self)
+            self.__class__._already_injected = True
+        elif inject:
+            logger.warning('A Clark object has already injected into the builtins module')
+        self._post_endpoint = kwargs.get('post_endpoint', 'report-error')
+        self._show_report_id = kwargs.get('show_report_id', True)
+        self._allow_logging = kwargs.get('allow_logging', True)
 
-    @classmethod
-    def report(cls, error, *args, **kwargs):
-
-        if not cls._ready:
-            cls._suitup()
+    def report(self, error, *args, **kwargs):
 
         def try_dump(obj):
             if isinstance(obj, types.ModuleType):
@@ -38,6 +43,24 @@ class WatchDog(object):
         report = {
             'locals': {name: try_dump(data) for name, data in lcls.items()},
             'globals': {name: try_dump(data) for name, data in glbls.items()},
-            'stack-trace': error
+            'stack-trace': error,
+            'os': ''
         }
-        print(report)
+        report = json.loads(utils.anonymize(json.dumps(report)))
+        self._send(report)
+
+    def _send(self, report_dict):
+        try:
+            reply = requests.post('{}/{}'.format(self._hostname, self._post_endpoint), data=report_dict)
+        except Exception as e:
+            logger.error(e)
+            logger.critical('Could not send error report')
+            return
+        if self._allow_logging:
+            if reply.status_code == 200:
+                if self._show_report_id:
+                    logger.info('Successfully sent error report. Report ID: {}'.format(reply.json()['id']))
+                else:
+                    logger.info('Sucessfully sent error report')
+            else:
+                logger.warning("Report was not sent successfully, code: {} reason: {}".format(reply.status_code, reply.reason))
